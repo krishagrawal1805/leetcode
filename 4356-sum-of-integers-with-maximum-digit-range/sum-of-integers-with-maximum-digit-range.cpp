9ccb88045165de6e3ca44d26b5ea9395480d2c2e
krishagrawal1805 <krishagrawal98275@gmail.com>
2026-07-08 23:31:13 +0530

    Time: 0 ms (100.00%) | Memory: 30.8 MB (98.52%) - LeetSync

--- 4356-sum-of-integers-with-maximum-digit-range/sum-of-integers-with-maximum-digit-range.cpp
+++ 4356-sum-of-integers-with-maximum-digit-range/sum-of-integers-with-maximum-digit-range.cpp
@@ -1,39 +1,32 @@
 class Solution {
 public:
     int maxDigitRange(vector<int>& nums) {
+        int maxrange=-1;
+        int sum=0;
         int n = nums.size();
-        vector<int> diff(n);
-        vector<int> arr(n);
-
         for (int i = 0; i < n; i++) {
-            int max = INT_MIN;
-            int min = INT_MAX;
-            arr[i] = nums[i];
-            while (nums[i] > 0) {
-                int num = nums[i] % 10;
-                nums[i] = nums[i] / 10;
+            int max_d = 0;
+            int min_d = 9;
+            int x= nums[i];
+            while (x > 0) {
+                int num = x % 10;
+                x = x / 10;
                 int c = num;
-                if (c >= max) {
-                    max = c;
-                }
-                if (c <= min) {
-                    min = c;
-                }
-                diff[i] = max - min;
+                max_d=max(c,max_d);
+                min_d=min(c,min_d);
+                
             }
-        }
-        int a = diff[0];
-        for (int i = 0; i < diff.size(); i++) {
-            if (a <= diff[i]) {
-                a = diff[i];
+            int range=max_d-min_d;
+            if(range>maxrange){
+                maxrange=range;
+                sum=nums[i];
             }
-        }
-        int ans = 0;
-        for (int i = 0; i < diff.size(); i++) {
-            if (a == diff[i]) {
-                ans += arr[i];
+            else if(range==maxrange){
+                sum+=nums[i];
             }
+
         }
-        return ans;
+        
+        return sum;
     }
 };
